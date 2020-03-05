79db9fdac7f97d859735ae59093bf5aa7f2f462b
Stepos36 <stepos36@gmail.com>
2020-03-05 17:05:00 -0500

    homework #1: sign up button toggler added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,14 @@ import { AuthGuard } from './common/auth/auth.guard';
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: '**', component: HomeComponent, canActivate: [AuthGuard] },
+    // { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    // { path: '**', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'home', component: HomeComponent},
+    { path: '**', component: HomeComponent},
 ];
 
 @NgModule({
+
     exports: [RouterModule],
     imports: [RouterModule.forRoot(appRoutes)],
 })
--- src/app/common/auth/login/login.component.ts
+++ src/app/common/auth/login/login.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
     templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
     userName = '';
     password = '';
+    showSignup = false;
 
     constructor(private router: Router, private authService: AuthService) {}
+
+    ngOnInit(): void {
+        console.log('comes into being');
+    }
+
+    ngOnDestroy(): void {
+        console.log('is destroyed');
+    }
+
+    toggleSignup(): void {
+        this.showSignup = !this.showSignup;
+        this.userName ='';
+        this.password =''
+    }
+
+    signup(): void {
+        const payload = {
+            userName: this.userName,
+            password: this.password,
+        };
+        console.log(payload);
+    }
 
     login(): void {
         this.authService.login(this.userName, this.password).subscribe(
